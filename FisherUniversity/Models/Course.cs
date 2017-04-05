b77096b13bd88aa552ab7ccad48cebc27066cc07
Bill Matera <matera@optonline.net>
2017-04-05 19:20:25 -0400

    Tutorial 2, Stopped at Update the Create page

---
+++ FisherUniversity/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FisherUniversity.Models
+{
+    public class Course
+    {
+        //The next statement lets you enter the primary key for the course rather than having the database generate it.
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
